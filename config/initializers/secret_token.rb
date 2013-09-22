95bc41e3997a044c044531ddfb6775f7069cfa91
Brandon Cummings <brandon.cummings@kevy.com>
2013-09-21 22:32:15 -0400

    Add a full_title helper

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '3ded7c63e8fa0dd4a277764e4e0f3887d43b3673960f557b1667ecfdc1e7ffefa0d4a4dcf6873342be58e13cc59b6067d94b2d819cb2927352a4c4bb7654bfc3'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
   
-  get "static_p_ages/contact"
+ 
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
